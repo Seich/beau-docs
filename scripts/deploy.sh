c58e7937195664ff546f4fb6b014ae740039b4d3
David Diaz <seich@martianwabbit.com>
2018-01-21 02:49:47 -0600

    Added documentation for hosts.
    Added automatic deployments with circleCI.

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+git config --global user.email "CircleCi"
+git config --global user.name "CircleCi"
+
+git clone $CIRCLE_REPOSITORY_URL out
+
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git rm -rf .
+cd ..
+
+harp compile
+cp -a www/. out/.
+cd out
+
+git add -A
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+git push origin $TARGET_BRANCH
